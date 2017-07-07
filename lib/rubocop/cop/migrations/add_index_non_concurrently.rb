af5bde60b4cb51abc6e0d54009533eedfadc70f9
Peter Graham <peterghm@gmail.com>
2017-07-07 11:19:56 -0400

    handle add_reference correctly

--- lib/rubocop/cop/migrations/add_index_non_concurrently.rb
+++ lib/rubocop/cop/migrations/add_index_non_concurrently.rb
@@ -4,31 +4,61 @@ module RuboCop
       class AddIndexNonConcurrently < Cop
         MSG = 'Use `algorithm: :concurrently` to avoid locking database.'.freeze
 
-        def_node_matcher :add_index_or_add_reference?, <<-PATTERN
-          (send nil {:add_index :add_reference} ...)
+        def_node_matcher :add_index_match, <<-PATTERN
+          (send nil :add_index _ _ (hash $...))
+        PATTERN
+
+        def_node_matcher :add_reference_match, <<-PATTERN
+          (send nil {:add_reference :add_belongs_to} _ _ (hash $...))
+        PATTERN
+
+        def_node_matcher :add_reference_index_options?, <<-PATTERN
+          (pair (sym :index) !({:nil false}))
         PATTERN
 
         def on_send(node)
-          if add_index_or_add_reference?(node)
-            options = options_hash(node)
-            if !options[:algorithm].present? || options[:algorithm] != "concurrently"
-              add_offense(node, :expression, MSG)
-            end
+          if node.method_name == :add_index
+            check_add_index(node)
+          elsif [:add_reference, :add_belongs_to].include?(node.method_name)
+            check_add_reference(node)
           end
         end
 
         private
 
-        def options_hash(node)
-          options = {}
-          node.children.each do |c|
-            if c.is_a?(AST::Node) && c.type == :hash
-              c.each_pair do |key_node, value_node|
-                options[key_node.children.first.to_sym] = value_node.children.first.to_s
-              end
-            end
+        def check_add_index(node)
+          pairs = add_index_match(node)
+          if !pairs_contains_algorithm_concurrently?(pairs)
+            add_offense(node, :expression, MSG)
           end
-          options
+        end
+
+        def check_add_reference(node)
+          pairs = add_reference_match(node)
+          index_options = pairs&.find { |pair| add_reference_index_options?(pair) }&.value
+          return unless index_options
+
+          valid = true
+          if index_options.true_type?
+            # `index: true` is always invalid
+            valid = false
+          elsif index_options.hash_type?
+            # check for `algorithm: :concurrently`
+            valid = pairs_contains_algorithm_concurrently?(index_options.pairs)
+          end
+          add_offense(node, :expression, MSG) if !valid
+        end
+
+        def pairs_contains_algorithm_concurrently?(pairs)
+          return false unless pairs.is_a?(Array)
+          pairs.each do |pair|
+            next unless pair.pair_type?
+            key = pair.key
+            value = pair.value
+            next unless (key.sym_type? || key.str_type?) && key.children.first.to_s == "algorithm"
+            return true if value.children.first.to_s == "concurrently"
+          end
+          false
         end
       end
     end
